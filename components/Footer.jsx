48c09531757fa2cb67dd9556f590be23ff73bb3f
Hassan Aftab <87358302+hassannaftabb@users.noreply.github.com>
2022-06-11 07:48:05 -0700

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -12,22 +12,22 @@ const Footer = () => {
     <div className="footer-container">
       <p>2022 Hassan Aftab ECom - Demo. All Rights Reserverd.</p>
       <p className="icons">
-        <Link href="" passHref>
+        <Link href="https://www.instagram.com/heyitshassann" passHref>
           <a>
             <AiFillInstagram />
           </a>
         </Link>
-        <Link href="" passHref>
+        <Link href="https://twitter.com/hassannaftabb" passHref>
           <a>
             <AiOutlineTwitter />
           </a>
         </Link>
-        <Link href="" passHref>
+        <Link href="https://github.com/hassannaftabb" passHref>
           <a>
             <AiOutlineGithub />
           </a>
         </Link>
-        <Link href="" passHref>
+        <Link href="https://t.co/euh97iAieb" passHref>
           <a>
             <AiOutlineLinkedin />
           </a>
